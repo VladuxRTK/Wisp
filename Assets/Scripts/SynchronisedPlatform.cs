c0b1364cabc18240d27c313147275e625d4ae1c8
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2021-04-02 11:22:38 +0300

    initial commit

---
+++ Assets/Scripts/SynchronisedPlatform.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynchronisedPlatform : MonoBehaviour
+{
+
+    public List<GameObject> platforms;
+    public List<Transform> initialPoints;
+    public float speed;
+    public bool fallEven;
+
+    public float fallEvenTimer;
+    private float startFallEvenTimer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        fallEven = true;
+        startFallEvenTimer = fallEvenTimer;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(FallPlatforms());
+
+        if(fallEvenTimer<=0)
+        {
+            if(fallEven)
+            {
+              
+                    
+                        platforms[0].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                        platforms[2].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                // platforms[i].gameObject.transform.position = Vector2.MoveTowards(platforms[i].gameObject.transform.position, initialPoints[i].position, speed * Time.deltaTime);
+
+
+
+                        platforms[1].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                        platforms[3].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                        platforms[1].gameObject.transform.position = Vector2.MoveTowards(platforms[1].gameObject.transform.position, initialPoints[1].position, speed * Time.deltaTime);
+                        platforms[3].gameObject.transform.position = Vector2.MoveTowards(platforms[3].gameObject.transform.position, initialPoints[1].position, speed * Time.deltaTime);
+                //fallEven = false;
+
+            }
+            if(!fallEven)
+            {
+
+
+                platforms[1].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                platforms[3].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                // platforms[i].gameObject.transform.position = Vector2.MoveTowards(platforms[i].gameObject.transform.position, initialPoints[i].position, speed * Time.deltaTime);
+
+
+
+                platforms[0].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                platforms[2].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                platforms[0].gameObject.transform.position = Vector2.MoveTowards(platforms[0].gameObject.transform.position, initialPoints[0].position, speed * Time.deltaTime);
+                platforms[2].gameObject.transform.position = Vector2.MoveTowards(platforms[2].gameObject.transform.position, initialPoints[2].position, speed * Time.deltaTime);
+               // fallEven = true;
+
+
+            }
+            fallEvenTimer = startFallEvenTimer;
+           
+        }
+        else
+        {
+            fallEvenTimer -= Time.deltaTime;
+            fallEven = !fallEven;
+        }
+    }
+
+    private IEnumerator FallPlatforms()
+    {
+        /*if(fallEven)
+        {
+            for(int i = 0;i<platforms.Count;i++)
+            {
+                if(i%2==0)
+                {
+                    platforms[i].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                 
+                   
+                }
+            }
+            yield return new WaitForSeconds(2f);
+            for(int i = 0; i < platforms.Count; i++)
+            {
+                platforms[i].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                platforms[i].gameObject.transform.position = Vector2.MoveTowards(platforms[i].gameObject.transform.position, initialPoints[i].position, speed * Time.deltaTime);
+                fallEven = !fallEven;
+            }
+           
+        }
+        if(!fallEven)
+        {
+            for (int i = 0; i < platforms.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    platforms[i].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                    
+
+                }
+            }
+            yield return new WaitForSeconds(2f);
+            for (int i = 0; i < platforms.Count; i++)
+            {
+                platforms[i].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                platforms[i].gameObject.transform.position = Vector2.MoveTowards(platforms[i].gameObject.transform.position, initialPoints[i].position, speed * Time.deltaTime);
+                fallEven = !fallEven;
+            }
+        }*/
+
+        platforms[0].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        platforms[1].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        yield return new WaitForSeconds(2f);
+        platforms[0].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        platforms[1].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        platforms[0].gameObject.transform.position = Vector2.MoveTowards(platforms[0].gameObject.transform.position, initialPoints[0].position, speed * Time.deltaTime);
+        platforms[1].gameObject.transform.position = Vector2.MoveTowards(platforms[1].gameObject.transform.position, initialPoints[1].position, speed * Time.deltaTime);
+        platforms[2].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        platforms[3].gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        yield return new WaitForSeconds(2f);
+        platforms[2].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        platforms[3].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        platforms[2].gameObject.transform.position = Vector2.MoveTowards(platforms[2].gameObject.transform.position, initialPoints[2].position, speed * Time.deltaTime);
+        platforms[3].gameObject.transform.position = Vector2.MoveTowards(platforms[3].gameObject.transform.position, initialPoints[3].position, speed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/TriggeringPlatform.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeringPlatform : MonoBehaviour
+{
+    public Transform targetPoint;
+
+    private Vector2 initPoint;
+    private Rigidbody2D rb;
+    private bool shouldFall;
+    public bool hasFallen;
+    public bool ableToFall;
+    public float speed;
+    public bool hasLanded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        initPoint = this.transform.position;
+        shouldFall = true;
+        hasFallen = false;
+        ableToFall = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, targetPoint.position, 20f);
+        /*if (hit)
+        {
+            if (hit.collider.CompareTag("Player") && shouldFall)
+            {
+                shouldFall = false;
+                StartCoroutine(Fall());
+
+            }
+            if (has)
+            {
+                this.transform.position = Vector2.MoveTowards(this.transform.position, initPoint, speed * Time.deltaTime);
+            }
+            if (this.transform.position.x == initPoint.x && initPoint.y == this.transform.position.y)
+            {
+                shouldFall = true;
+            }
+        }*/
+        if(hasFallen)
+        {
+            this.transform.position = Vector2.MoveTowards(this.transform.position, initPoint, speed * Time.deltaTime);
+        }
+        if (this.transform.position.x == initPoint.x && initPoint.y == this.transform.position.y)
+        {
+            ableToFall = true;
+            hasFallen = false;
+            
+        }
+        else
+        {
+            ableToFall = false;
+            
+        }
+        if(hasLanded)
+        {
+            StartCoroutine(GoBack());
+        }
+       
+    }
+
+    private IEnumerator Fall()
+    {
+        rb.isKinematic = false;
+        yield return new WaitForSeconds(0.3f);
+        rb.isKinematic = true;
+
+        // shouldFall = true;
+
+
+    }
+
+    public void SetRb(bool value)
+    {
+        rb.isKinematic = value;
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Ground"))
+        {
+            hasLanded = true;
+            rb.isKinematic = true;
+          
+            
+        }
+    }*/
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            hasLanded = true;
+            rb.isKinematic = true;
+        }
+    }
+
+    // Go back to initial Point after some time
+    private IEnumerator GoBack()
+    {
+        yield return new WaitForSeconds(1f);
+        hasFallen = true;
+        hasLanded = false;
+    }
+
+}
